cf96a6401d2adceffbc5f472af4edfe3b9e1f419
Humaiza Naz <159733464+HumaizaNaz@users.noreply.github.com>
2025-01-01 01:34:05 +0500

    update

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { blog } from './blog'
 import { comment } from './comment'
-import { tag } from './tag'
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blog,comment,tag],
+  types: [blog,comment,],
 }
